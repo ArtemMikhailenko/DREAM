f68ce02545f68169f8139cedad1cc88e1fd7f86e
ArtemMikhailenko <Artemlend@gmail.com>
2026-07-27 13:02:47 +0300

    Add LinkedIn Insight Tag to the analytics loader
    Env-gated on NEXT_PUBLIC_LINKEDIN_PARTNER_ID, same pattern as the Meta and
    TikTok pixels. Renders nothing until the partner id is set.

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -6,6 +6,7 @@ const googleAdsId = process.env.NEXT_PUBLIC_GOOGLE_ADS_ID;
 const enableDirectGoogleAds =
   process.env.NEXT_PUBLIC_GOOGLE_ADS_DIRECT_TRACKING === "true";
 const tiktokPixelId = process.env.NEXT_PUBLIC_TIKTOK_PIXEL_ID;
+const linkedInPartnerId = process.env.NEXT_PUBLIC_LINKEDIN_PARTNER_ID;
 
 export function Analytics() {
   return (
@@ -50,6 +51,23 @@ export function Analytics() {
         <Script id="tiktok-pixel" strategy="afterInteractive">
           {`!function(w,d,t){w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=['page','track','identify','instances','debug','on','off','once','ready','alias','group','enableCookie','disableCookie','holdConsent','revokeConsent','grantConsent'];ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.load=function(e,n){var r='https://analytics.tiktok.com/i18n/pixel/events.js',o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement('script');n.type='text/javascript';n.async=!0;n.src=r+'?sdkid='+e+'&lib='+t;e=document.getElementsByTagName('script')[0];e.parentNode.insertBefore(n,e)};ttq.load('${tiktokPixelId}');ttq.page();}(window,document,'ttq');`}
         </Script>
+      ) : null}
+
+      {linkedInPartnerId ? (
+        <>
+          <Script id="linkedin-insight" strategy="afterInteractive">
+            {`window._linkedin_data_partner_ids=window._linkedin_data_partner_ids||[];window._linkedin_data_partner_ids.push('${linkedInPartnerId}');(function(l){if(!l){window.lintrk=function(a,b){window.lintrk.q.push([a,b])};window.lintrk.q=[]}var s=document.getElementsByTagName('script')[0];var b=document.createElement('script');b.type='text/javascript';b.async=true;b.src='https://snap.licdn.com/li.lms-analytics/insight.min.js';s.parentNode.insertBefore(b,s);})(window.lintrk);`}
+          </Script>
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              alt=""
+              src={`https://px.ads.linkedin.com/collect/?pid=${linkedInPartnerId}&fmt=gif`}
+            />
+          </noscript>
+        </>
       ) : null}
     </>
   );
